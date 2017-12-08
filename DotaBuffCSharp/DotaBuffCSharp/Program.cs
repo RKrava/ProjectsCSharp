540cd2d864eede026c4f54d337fef51224fc38de
Roma <krab9887@gmail.com>
2017-12-08 21:32:00 +0200

    S

--- DotaBuffCSharp/DotaBuffCSharp/Program.cs
+++ DotaBuffCSharp/DotaBuffCSharp/Program.cs
@@ -8,8 +8,39 @@ namespace DotaBuffCSharp
 {
     class Program
     {
+        public string[] nick_radiant = new string[5];
+        public string[] nick_dire = new string[5];
+
+        class Player
+        {
+            public string name;
+            public int[] number_games; 
+        }
+
+        class Team {
+            public void Get_Radiant()
+            {
+                foreach (string i in nick_dire)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            public void Get_Dire()
+            {
+                foreach (string i in nick_radiant)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        class Game
+        {
+            public int number;
+            
+        }
         static void Main(string[] args)
         {
+
         }
     }
 }
